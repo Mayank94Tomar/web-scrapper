89757574f39d0bc7ffc34e828c1f20a9ad8477f5
Mayank94Tomar <maktom.learning@gmail.com>
2025-05-07 11:41:48 -0400

    added code to complete assigment

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -2,32 +2,34 @@
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
 import os
+import sys
+
 import requests
 import re
 # Code here - Import BeautifulSoup library
-
 # Code ends here
+from bs4 import BeautifulSoup
+
 
 # function to get the html source text of the medium article
 def get_page():
-	global url
-	
-	# Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
-	
-	# Code ends here
-	
-	# handling possible error
-	if not re.match(r'https?://medium.com/',url):
-		print('Please enter a valid website, or make sure it is a medium article')
-		sys.exit(1)
+    global url
+    
+    # Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
+    # Code ends here
+    url = input("Enter url of a medium article: ")
 
-	# Code here - Call get method in requests object, pass url and collect it in res
-	
-	# Code ends here
+    # handling possible error
+    if not re.match(r'https?://medium.com/',url):
+        print('Please enter a valid website, or make sure it is a medium article')
+        sys.exit(1)
 
-	res.raise_for_status()
-	soup = BeautifulSoup(res.text, 'html.parser')
-	return soup
+    # Code here - Call get method in requests object, pass url and collect it in res
+    # Code ends here
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, 'html.parser')
+    return soup
 
 # function to remove all the html tags and replace some with specific strings
 def clean(text):
@@ -35,36 +37,35 @@ def clean(text):
     rep = dict((re.escape(k), v) for k, v in rep.items()) 
     pattern = re.compile("|".join(rep.keys()))
     text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
-    text = re.sub('\<(.*?)\>', '', text)
+    text = re.sub(r'<(.*?)>', '', text)
     return text
 
 
 def collect_text(soup):
-	text = f'url: {url}\n\n'
-	para_text = soup.find_all('p')
-	print(f"paragraphs text = \n {para_text}")
-	for para in para_text:
-		text += f"{para.text}\n\n"
-	return text
+    text = f'url: {url}\n\n'
+    para_text = soup.find_all('p')
+    print(f"paragraphs text = \n {para_text}")
+    for para in para_text:
+        text += f"{para.text}\n\n"
+    return text
 
 # function to save file in the current directory
 def save_file(text):
-	if not os.path.exists('./scraped_articles'):
-		os.mkdir('./scraped_articles')
-	name = url.split("/")[-1]
-	print(name)
-	fname = f'scraped_articles/{name}.txt'
-	
-	# Code here - write a file using with (2 lines)
-	
-
-	# Code ends here
-
-	print(f'File saved in directory {fname}')
+    if not os.path.exists('./scraped_articles'):
+        os.mkdir('./scraped_articles')
+    name = url.split("/")[-1]
+    print(name)
+    fname = f'scraped_articles/{name}.txt'
+    
+    # Code here - write a file using with (2 lines)
+    print(f'File saved in directory {fname}')
+    # Code ends here
+    with open(fname, 'w', encoding='utf-8') as f:
+        f.write(text)
 
 
 if __name__ == '__main__':
-	text = collect_text(get_page())
-	save_file(text)
-	# Instructions to Run this python code
-	# Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
+    text = collect_text(get_page())
+    save_file(text)
+    # Instructions to Run this python code
+    # Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
