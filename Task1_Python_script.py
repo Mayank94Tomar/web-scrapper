f7259cf223d1dce395f329cde52ae212d84b4cda
Mayank94Tomar <maktom.learning@gmail.com>
2025-05-07 15:51:52 -0400

    final commit

--- Task1_Python_script.py
+++ Task1_Python_script.py
@@ -2,10 +2,10 @@
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
 import os
+import re
 import sys
 
 import requests
-import re
 # Code here - Import BeautifulSoup library
 # Code ends here
 from bs4 import BeautifulSoup
@@ -14,13 +14,13 @@ from bs4 import BeautifulSoup
 # function to get the html source text of the medium article
 def get_page():
     global url
-    
+
     # Code here - Ask the user to input "Enter url of a medium article: " and collect it in url
     # Code ends here
     url = input("Enter url of a medium article: ")
 
     # handling possible error
-    if not re.match(r'https?://medium.com/',url):
+    if not re.match(r'https?://medium.com/', url):
         print('Please enter a valid website, or make sure it is a medium article')
         sys.exit(1)
 
@@ -31,10 +31,11 @@ def get_page():
     soup = BeautifulSoup(res.text, 'html.parser')
     return soup
 
+
 # function to remove all the html tags and replace some with specific strings
 def clean(text):
-    rep = {"<br>": "\n", "<br/>": "\n", "<li>":  "\n"}
-    rep = dict((re.escape(k), v) for k, v in rep.items()) 
+    rep = {"<br>": "\n", "<br/>": "\n", "<li>": "\n"}
+    rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile("|".join(rep.keys()))
     text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
     text = re.sub(r'<(.*?)>', '', text)
@@ -49,6 +50,7 @@ def collect_text(soup):
         text += f"{para.text}\n\n"
     return text
 
+
 # function to save file in the current directory
 def save_file(text):
     if not os.path.exists('./scraped_articles'):
@@ -56,12 +58,12 @@ def save_file(text):
     name = url.split("/")[-1]
     print(name)
     fname = f'scraped_articles/{name}.txt'
-    
+
     # Code here - write a file using with (2 lines)
     print(f'File saved in directory {fname}')
     # Code ends here
-    with open(fname, 'w', encoding='utf-8') as f:
-        f.write(text)
+    with open(fname, 'w', encoding='utf-8') as ftext:
+        ftext.write(text)
 
 
 if __name__ == '__main__':
